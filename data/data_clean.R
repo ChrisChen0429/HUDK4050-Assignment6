dcc14700c06accd733d9adc5a9228bd65c1cc29b
Yi Chen <yc3356@columbia.edu>
2020-12-07 16:25:56 +0800

    data clean

--- data/data_clean.R
+++ data/data_clean.R
@@ -2,3 +2,8 @@ library(tidyverse)
 XuechunLi <- read_csv("XuechunLi.csv")
 XuechunLi$time <- round(difftime(XuechunLi$time, XuechunLi$time[1],units = 'secs'),3)
 write_csv(XuechunLi,"XuechunLi.csv")
+
+
+XuechunLi <- read_csv("YunzhaoWu.csv")
+XuechunLi$time <- round(difftime(XuechunLi$time, XuechunLi$time[1],units = 'secs'),3)
+write_csv(XuechunLi,"YunzhaoWu.csv")
