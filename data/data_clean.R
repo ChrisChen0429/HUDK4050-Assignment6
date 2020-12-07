4e34898a825d419ba9c87134a34e3c4e91afe1f0
Yi Chen <yc3356@columbia.edu>
2020-12-07 22:13:09 +0800

    data

--- data/data_clean.R
+++ data/data_clean.R
@@ -7,3 +7,7 @@ write_csv(XuechunLi,"XuechunLi.csv")
 XuechunLi <- read_csv("YunzhaoWu.csv")
 XuechunLi$time <- round(difftime(XuechunLi$time, XuechunLi$time[1],units = 'secs'),3)
 write_csv(XuechunLi,"YunzhaoWu.csv")
+
+TianyuChang <- read_csv("TianyuChang.csv")
+TianyuChang$time <- round(difftime(TianyuChang$time, TianyuChang$time[1],units = 'secs'),3)
+write_csv(TianyuChang,"TianyuChang.csv")
